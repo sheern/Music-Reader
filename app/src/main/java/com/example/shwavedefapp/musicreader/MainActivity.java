6219c8a53c2aca77e471a18a2708f506cc77ed14
shwavedefapp <shaanp2@illinois.edu>
2017-01-05 03:05:27 -0600

    cancel dialog, added programmatic linear layout

--- app/src/main/java/com/example/shwavedefapp/musicreader/MainActivity.java
+++ app/src/main/java/com/example/shwavedefapp/musicreader/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -14,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     SeekBar mMetronomeBar;
     TextView mDynamicBPM;
     Button mAddMusic;
+    LinearLayout mListSheets;
+
+    EditText mSheetName;
+    Button mDialogCreate;
+    Button mDialogCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
                 dialogSetup();
             }
         });
+        mListSheets = (LinearLayout) findViewById(R.id.list_sheets);
     }
 
     public void metronome() {
@@ -70,5 +78,15 @@ public class MainActivity extends AppCompatActivity {
                 .setTitle(getResources().getString(R.string.dialog_title))
                 .setView(R.layout.addsheet_dialog).create();
         addMusicDialog.show();
+
+        mDialogCreate = (Button) addMusicDialog.findViewById(R.id.dialog_create);
+        mDialogCancel = (Button) addMusicDialog.findViewById(R.id.dialog_cancel);
+
+        mDialogCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addMusicDialog.cancel();
+            }
+        });
     }
 }
