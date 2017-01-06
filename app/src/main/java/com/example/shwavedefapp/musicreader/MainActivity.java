9e89ed7e8a117a4b4be25d72ee25868a5643790b
shwavedefapp <shaanp2@illinois.edu>
2017-01-06 02:16:56 -0600

    dialog creates buttons, work on pdfview

--- app/src/main/java/com/example/shwavedefapp/musicreader/MainActivity.java
+++ app/src/main/java/com/example/shwavedefapp/musicreader/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.shwavedefapp.musicreader;
 
 import android.app.Dialog;
+import android.graphics.drawable.Drawable;
+import android.graphics.pdf.PdfDocument;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,11 +14,14 @@ import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
     SeekBar mMetronomeBar;
     TextView mDynamicBPM;
     Button mAddMusic;
+    TextView mNoMusicHint;
     LinearLayout mListSheets;
 
     EditText mSheetName;
@@ -27,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         metronome();
+        mNoMusicHint = (TextView) findViewById(R.id.no_music_text);
+        mListSheets = (LinearLayout) findViewById(R.id.list_sheets);
         mAddMusic = (Button) findViewById(R.id.add_music_button);
         mAddMusic.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
                 dialogSetup();
             }
         });
-        mListSheets = (LinearLayout) findViewById(R.id.list_sheets);
     }
 
     public void metronome() {
@@ -79,14 +86,30 @@ public class MainActivity extends AppCompatActivity {
                 .setView(R.layout.addsheet_dialog).create();
         addMusicDialog.show();
 
+        mSheetName = (EditText) addMusicDialog.findViewById(R.id.custom_file_name);
         mDialogCreate = (Button) addMusicDialog.findViewById(R.id.dialog_create);
         mDialogCancel = (Button) addMusicDialog.findViewById(R.id.dialog_cancel);
 
+        mDialogCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onDialogCreateNewSheet();
+                addMusicDialog.cancel();
+            }
+        });
         mDialogCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 addMusicDialog.cancel();
             }
         });
+    }
+
+    private void onDialogCreateNewSheet() {
+        Button newSheet = new Button(getApplicationContext());
+        newSheet.setText(mSheetName.getText().toString());
+        newSheet.setBackgroundResource(R.color.colorPrimary);
+        mListSheets.addView(newSheet);
+        if(mListSheets.getChildCount() == 2) mNoMusicHint.setVisibility(View.GONE);
     }
 }
